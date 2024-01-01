35a7262cd90963eb0ee699661d5e5abcc12c9cfe
Han Sol Jin <hansol@hansol.ca>
2024-01-01 14:18:28 -0800

    caymanlm: overlay-lineage: Setup Aperture overlay
    Change-Id: Id87d6ec65088ae3ff5445bc8d1e3651dbf505aef

--- device.mk
+++ device.mk
@@ -11,5 +11,9 @@ DEVICE_NAME := caymanlm
 # Inherit from the common device configuration.
 $(call inherit-product, device/lge/sm7250-common/sm7250-common.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay-lineage
+
 # Inherit from vendor makefiles.
 $(call inherit-product, vendor/lge/caymanlm/caymanlm-vendor.mk)
