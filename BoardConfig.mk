dc895db7f6745c887205f8e6c21e8c1ff8e048e7
Tuan Anh <tuan73176@gmail.com>
2025-02-01 09:32:45 +0700

    caymanlm: Add required system/vendor properties
    Change-Id: I2302463169b4959afcc60b026c5c8abf08e51bf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,5 +10,9 @@ include device/lge/sm7250-common/BoardConfigCommon.mk
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lineageos_cayman_defconfig
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit vendor BoardConfig
 include vendor/lge/caymanlm/BoardConfigVendor.mk
