e6ee50b5dcc46387a377dc74a62cf35ea6d32d5d
Tuan Anh <tuan73176@gmail.com>
2025-02-01 08:43:51 +0700

    caymanlm: Initial skeleton device makefiles
    Change-Id: I198de21d3734a58203e63c2c256082c080bebd68

---
+++ device.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/lge/caymanlm
+
+DEVICE_NAME := caymanlm
+
+# Inherit from the common device configuration.
+$(call inherit-product, device/lge/sm7250-common/sm7250-common.mk)
