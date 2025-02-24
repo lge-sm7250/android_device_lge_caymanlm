6ee1d95f0d70350b3ab86ea7b917ee6f2d26d99f
Han Sol Jin <hansol@hansol.ca>
2025-02-24 00:07:56 -0800

    caymanlm: add audio configs
    caymanlm-user 13 TKQ1.220829.002 231811419f557 release-keys
    
    Change-Id: If80af16f227825d3afa0ade074c87d1c01bb18e8

--- device.mk
+++ device.mk
@@ -11,6 +11,13 @@ DEVICE_NAME := caymanlm
 # Inherit from the common device configuration.
 $(call inherit-product, device/lge/sm7250-common/sm7250-common.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay-lineage
